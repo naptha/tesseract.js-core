b566423d16255036de5d7006f8165da3d36467eb
Jerome Wu <jeromewus@gmail.com>
2018-02-16 22:58:55 +0800

    Replace -v with --mount in scripts/run-container.sh

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -it -v $(pwd):/src trzeci/emscripten:sdk-tag-1.37.33-64bit /bin/bash
+docker run -it \
+  --mount type=bind,source=$(pwd),target=/src \
+  trzeci/emscripten:sdk-tag-1.37.33-64bit \
+  /bin/bash
