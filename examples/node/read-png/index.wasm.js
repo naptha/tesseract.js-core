8db4d6c373399347c85d3dec614c593dede2a38a
Jerome Wu <jeromewus@gmail.com>
2018-11-13 13:47:48 +0800

    Update read-png example

--- examples/node/read-png/index.wasm.js
+++ examples/node/read-png/index.wasm.js
@@ -11,16 +11,21 @@ TessModule.onRuntimeInitialized = () => {
 
   TessModule.FS.writeFile(`${lang}.traineddata`, fs.readFileSync(`../../traineddata/${lang}.traineddata`));
 
-  const img = fs.readFileSync('../../data/tyger.png');
+  const img = fs.readFileSync('../../data/testocr.png');
   const img_ptr = TessModule._malloc(img.length * Uint8Array.BYTES_PER_ELEMENT);
   TessModule.HEAPU8.set(img, img_ptr);
+  const img_pixa = TessModule._pixReadMem(img_ptr, img.length);
 
   api.Init(null, lang);
-  api.SetImage(TessModule._pixReadMem(img_ptr, img.length));
+  api.SetImage(img_pixa);
 
+  const width = TessModule.getValue(img_pixa, 'i32');
+  const height = TessModule.getValue(img_pixa+4, 'i32');
+
+  console.log(`image size: ${width} x ${height}`);
   console.log(api.GetUTF8Text());
 
   api.End();
   TessModule.destroy(api);
   TessModule._free(img_ptr);
-}
+};
