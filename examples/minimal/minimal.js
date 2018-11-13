0ccee1b65d5c86856a04467fd6e93c295e484757
Jerome Wu <jeromewus@gmail.com>
2018-11-13 17:48:21 +0800

    Update minimal.js

--- examples/minimal/minimal.js
+++ examples/minimal/minimal.js
@@ -1,27 +1,26 @@
-const lang = 'eng';
-
 const fs = require('fs');
-const TesseractCore = require('../../');
+const TesseractCore = require('../../tesseract/build/bin/tesseract-asm')();
 const { width, height, data } = require('./test-image');
 
-const Module = TesseractCore();
-const tessAPI = new Module.TessBaseAPI();
-
+const lang = 'eng';
+const tessAPI = new TesseractCore.TessBaseAPI();
 const buf = fs.readFileSync(`../traineddata/${lang}.traineddata`);
 
-Module.FS.mkdir('/tessdata');
-Module.FS.writeFile(`/tessdata/${lang}.traineddata`, buf);
+TesseractCore.FS.mkdir('/tessdata');
+TesseractCore.FS.writeFile(`/tessdata/${lang}.traineddata`, buf);
 
-const ptr = Module._malloc(data.length * Uint8Array.BYTES_PER_ELEMENT);
+// const ptr = TesseractCore._malloc(data.length * Uint8Array.BYTES_PER_ELEMENT);
 
-Module.HEAPU8.set(data, ptr);
+// TesseractCore.HEAPU8.set(data, ptr);
 
-tessAPI.Init(null, lang);
-tessAPI.SetImage(ptr, width, height, Uint8Array.BYTES_PER_ELEMENT, width);
-tessAPI.SetRectangle(0, 0, width, height);
+tessAPI.Init('/tessdata', lang);
+// tessAPI.SetImage(ptr, width, height, Uint8Array.BYTES_PER_ELEMENT, width);
+// tessAPI.SetRectangle(0, 0, width, height);
 
+/*
 console.log(tessAPI.GetUTF8Text());
+*/
 
-tessAPI.End();
-Module.destroy(tessAPI);
-Module._free(ptr);
+// tessAPI.End();
+// TesseractCore.destroy(tessAPI);
+// TesseractCore._free(ptr);
