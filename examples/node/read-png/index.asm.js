95b4c03772862c3b1acf56dc3ea92ae2816d6b8c
Jerome Wu <jeromewus@gmail.com>
2018-11-13 12:21:03 +0800

    Add examples for canvas and read from png

---
+++ examples/node/read-png/index.asm.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const TessModule = require('../../../tesseract/build/bin/tesseract-asm')({
+  TesseractProgress(percent) {
+    console.log(`Progress: ${percent}`);
+  }
+});
+const lang = 'eng';
+const api = new TessModule.TessBaseAPI();
+
+TessModule.FS.writeFile(`${lang}.traineddata`, fs.readFileSync(`../../traineddata/${lang}.traineddata`));
+
+const img = fs.readFileSync('../../data/tyger.png');
+const img_ptr = TessModule._malloc(img.length * Uint8Array.BYTES_PER_ELEMENT);
+TessModule.HEAPU8.set(img, img_ptr);
+
+api.Init(null, lang);
+api.SetImage(TessModule._pixReadMem(img_ptr, img.length));
+
+console.log(api.GetUTF8Text());
+
+api.End();
+TessModule.destroy(api);
+TessModule._free(img_ptr);
