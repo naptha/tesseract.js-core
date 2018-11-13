be6ab5b231dc20ee263a1494492fee871819033c
Jerome Wu <jeromewus@gmail.com>
2018-11-13 08:47:50 +0800

    Update tesseract and examples

--- examples/minimal/minimal.js
+++ examples/minimal/minimal.js
@@ -6,21 +6,18 @@ const lang = 'eng';
 const tessAPI = new TesseractCore.TessBaseAPI();
 const buf = fs.readFileSync(`../traineddata/${lang}.traineddata`);
 
-TesseractCore.FS.mkdir('/tessdata');
-TesseractCore.FS.writeFile(`/tessdata/${lang}.traineddata`, buf);
+TesseractCore.FS.writeFile(`${lang}.traineddata`, buf);
 
-// const ptr = TesseractCore._malloc(data.length * Uint8Array.BYTES_PER_ELEMENT);
+const ptr = TesseractCore._malloc(data.length * Uint8Array.BYTES_PER_ELEMENT);
 
-// TesseractCore.HEAPU8.set(data, ptr);
+TesseractCore.HEAPU8.set(data, ptr);
 
-tessAPI.Init('/tessdata', lang);
-// tessAPI.SetImage(ptr, width, height, Uint8Array.BYTES_PER_ELEMENT, width);
-// tessAPI.SetRectangle(0, 0, width, height);
+tessAPI.Init(null, lang);
+tessAPI.SetImage(ptr, width, height, Uint8Array.BYTES_PER_ELEMENT, width);
+tessAPI.SetRectangle(0, 0, width, height);
 
-/*
-console.log(tessAPI.GetUTF8Text());
-*/
+// console.log(tessAPI.GetUTF8Text());
 
-// tessAPI.End();
-// TesseractCore.destroy(tessAPI);
-// TesseractCore._free(ptr);
+tessAPI.End();
+TesseractCore.destroy(tessAPI);
+TesseractCore._free(ptr);
