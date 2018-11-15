190eae76899b5eca9d5333d8ca355f3e65ca7783
Jerome Wu <jeromewus@gmail.com>
2018-11-15 18:50:39 +0800

    Use mocha instead of jest to avoid out of memory issue

--- tests/unit/TessBaseAPI.test.js
+++ tests/unit/TessBaseAPI.test.js
@@ -1,13 +1,16 @@
+const expect = require('expect.js');
 const TessModuleASM = require('../../tesseract-core.asm')();
 const loadTrainedDataFromRemote = require('./utils/loadTrainedDataFromRemote');
 
 describe('TessBaseAPI.Init', () => {
-  test('Init', () => {
+  it('Init', () => {
     const api = new TessModuleASM.TessBaseAPI();
     return loadTrainedDataFromRemote('eng', TessModuleASM)
-      .then(() => {
+      .then((dataList) => {
         api.Init(null, 'eng');
-        expect(api.GetInitLanguagesAsString()).toBe('eng');
+        expect(dataList.length).to.be(1);
+        expect(dataList[0].length).not.to.be(0);
+        expect(api.GetInitLanguagesAsString()).to.be('eng');
       });
-  });
+  }).timeout(10000);
 });
--- tests/unit/utils.test.js
+++ tests/unit/utils.test.js
@@ -1,33 +1,31 @@
+const expect = require('expect.js');
 const loadTrainedDataFromRemote = require('./utils/loadTrainedDataFromRemote');
 
 describe('loadTrainedDataFromRemote', () => {
-  test('load 1 lang', () => {
-    expect.assertions(2);
-    return loadTrainedDataFromRemote('slk_frak')
+  it('load 1 lang', () => (
+    loadTrainedDataFromRemote('slk_frak')
       .then((langs) => {
-        expect(langs.length).toBe(1);
-        expect(langs[0].length).not.toBe(0);
-      });
-  });
+        expect(langs.length).to.be(1);
+        expect(langs[0].length).not.to.be(0);
+      })
+  ));
 
-  test('load 2 langs', () => {
-    expect.assertions(3);
-    return loadTrainedDataFromRemote('slk_frak+fas')
+  it('load 2 langs', () => (
+    loadTrainedDataFromRemote('slk_frak+fas')
       .then((langs) => {
-        expect(langs.length).toBe(2);
-        expect(langs[0].length).not.toBe(0);
-        expect(langs[1].length).not.toBe(0);
-      });
-  });
+        expect(langs.length).to.be(2);
+        expect(langs[0].length).not.to.be(0);
+        expect(langs[1].length).not.to.be(0);
+      })
+  ));
 
-  test('load 3 langs', () => {
-    expect.assertions(4);
-    return loadTrainedDataFromRemote('slk_frak+fas+mri')
+  it('load 3 langs', () => (
+    loadTrainedDataFromRemote('slk_frak+fas+mri')
       .then((langs) => {
-        expect(langs.length).toBe(3);
-        expect(langs[0].length).not.toBe(0);
-        expect(langs[1].length).not.toBe(0);
-        expect(langs[2].length).not.toBe(0);
-      });
-  });
+        expect(langs.length).to.be(3);
+        expect(langs[0].length).not.to.be(0);
+        expect(langs[1].length).not.to.be(0);
+        expect(langs[2].length).not.to.be(0);
+      })
+  ));
 });
