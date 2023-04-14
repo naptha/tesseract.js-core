5ed37373af75c962c363726547307a1816568993
Balearica <you@example.com>
2023-04-13 20:39:06 -0700

    Renamed wasm module to TesseractCore; removed asm version

--- tests/unit/utils/duplicateIt.js
+++ tests/unit/utils/duplicateIt.js
@@ -1,9 +1,7 @@
-const TesseractCoreASM = require('../../../tesseract-core.asm');
-const TesseractCoreWASM = require('../../../tesseract-core');
-const TesseractCoreWASMSIMD = require('../../../tesseract-core-simd');
+const TesseractCore = require('../../../tesseract-core');
+const TesseractCoreSIMD = require('../../../tesseract-core-simd');
 
 module.exports = (desc, callback, { timeout }) => {
-  it(`[ASM]: ${desc}`, callback(TesseractCoreASM)).timeout(timeout);
-  it(`[WASM]: ${desc}`, callback(TesseractCoreWASM)).timeout(timeout);
-  it(`[WASM-SIMD]: ${desc}`, callback(TesseractCoreWASMSIMD)).timeout(timeout);
+  it(`[WASM]: ${desc}`, callback(TesseractCore)).timeout(timeout);
+  it(`[WASM-SIMD]: ${desc}`, callback(TesseractCoreSIMD)).timeout(timeout);
 };
