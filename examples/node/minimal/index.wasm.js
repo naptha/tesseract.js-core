259a60882b0bf91a16020e243e5f13c03fa82e47
Balearica <admin@scribeocr.com>
2023-09-01 19:30:24 -0700

    Updated examples

--- examples/node/minimal/index.wasm.js
+++ examples/node/minimal/index.wasm.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
+const path = require('path');
+
 const TesseractCore = require('../../../tesseract-core');
-const { width, height, data } = require('../../data/test-image.json');
+
+const [,, imagePath] = process.argv;
 
 TesseractCore().then((TessModule) => {
   const lang = 'eng';
   const api = new TessModule.TessBaseAPI();
-  const buf = fs.readFileSync(`../../../tests/traineddata/${lang}.traineddata`);
-  const ptr = TessModule._malloc(data.length * Uint8Array.BYTES_PER_ELEMENT);
+
+  const buf = fs.readFileSync(path.resolve(__dirname, `../../../tests/traineddata/${lang}.traineddata`));
 
   TessModule.FS.writeFile(`${lang}.traineddata`, buf);
-  TessModule.HEAPU8.set(data, ptr);
 
   api.Init(null, lang);
-  api.SetImage(ptr, width, height, Uint8Array.BYTES_PER_ELEMENT, width);
-  api.SetRectangle(0, 0, width, height);
+
+  const fileBuf = fs.readFileSync(imagePath);
+  TessModule.FS.writeFile("/input", fileBuf);
+
+  api.SetImageFile();
 
   console.log(api.GetUTF8Text());
 
   api.End();
   TessModule.destroy(api);
-  TessModule._free(ptr);
 });
