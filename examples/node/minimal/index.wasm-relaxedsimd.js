4bfa90c9ff7106b369addfd59d5099dd6778aad6
Fanchen Kong <fanchen.kong@intel.com>
2025-12-04 11:34:01 +0800

    Enable WASM Relaxed SIMD build for tesseract.js-core
    This change adds optional support for building Tesseract with WebAssembly Relaxed SIMD, allowing tesseract.js-core to take advantage of the WASM relaxed integer dot-product instructions (i32x4.relaxed_dot_i8x16_i7x16_add).

---
+++ examples/node/minimal/index.wasm-relaxedsimd.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const TesseractCore = require('../../../tesseract-core-relaxedsimd');
+
+const [,, imagePath] = process.argv;
+
+TesseractCore().then((TessModule) => {
+  const lang = 'eng';
+  const api = new TessModule.TessBaseAPI();
+
+  const buf = fs.readFileSync(path.resolve(__dirname, `../../../tests/traineddata/${lang}.traineddata`));
+
+  TessModule.FS.writeFile(`${lang}.traineddata`, buf);
+
+  api.Init(null, lang);
+
+  const fileBuf = fs.readFileSync(imagePath);
+  TessModule.FS.writeFile('/input', fileBuf);
+
+  api.SetImageFile();
+
+  console.log(api.GetUTF8Text());
+
+  api.End();
+  TessModule.destroy(api);
+});
